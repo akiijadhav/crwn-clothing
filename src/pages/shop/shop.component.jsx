1d337d6bdb6138f4c5cc609ed8bc3763b8ce7774
akiijadhav <akiijadhav22@gmail.com>
2021-06-07 18:59:44 +0530

    adding shop page with collections-preview & collection-item

---
+++ src/pages/shop/shop.component.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import SHOP_DATA from './shop.data.js';
+import '../../components/collection-preview/collection-preview.component';
+import CollectionPreview from '../../components/collection-preview/collection-preview.component';
+const ShopPage = () => {
+
+    const collections = SHOP_DATA;
+
+    return (
+        <div className='shop-page'>
+            {
+                collections.map(({id, ...otherCollectionProps}) => (
+                    <CollectionPreview key={id} {...otherCollectionProps} />
+                ))
+            }
+        </div>
+    )
+}
+
+export default ShopPage;
